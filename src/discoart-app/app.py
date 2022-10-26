7ffe900033bd3852f3a601e963e5e11450cad61f
Irina Kogay <kogayie1995@gmail.com>
2022-10-26 15:33:22 +0400

    lint the code

--- src/discoart-app/app.py
+++ src/discoart-app/app.py
@@ -8,7 +8,8 @@ from lightning.app.storage import Drive
 class MasterpieceCreator(ServeGradio):
     inputs = [
       gr.components.Textbox(label="print your prompt here", elem_id="label"),
-      gr.components.Dropdown(label="choose you model",
+      gr.components.Dropdown(
+        label="choose you model",
         choices=[
           'PulpSciFiDiffusion',
           'pixel_art_diffusion_hard_256',
@@ -20,10 +21,14 @@ class MasterpieceCreator(ServeGradio):
           'portrait_generator_v001_ema_0.9999_1MM',
           'FeiArt_Handpainted_CG_Diffusion',
           'Ukiyo-e_Diffusion_All_V1.by_thegenerativegeneration'
-        ]),
+        ]
+      ),
       gr.components.Number(value=250, label="number of steps"),
     ]
-    outputs = gr.components.Image(type="auto", label="Your masterpiece will be here")
+    outputs = gr.components.Image(
+        type="auto",
+        label="Your masterpiece will be here"
+      )
     enable_queue = True
     css = '''
       #component-6 {
@@ -41,32 +46,37 @@ class MasterpieceCreator(ServeGradio):
         font-size: 15px;
         border-radius: 6px;
       }
-      .gr-button-primary {
-        background: linear-gradient(206.91deg, rgb(121, 46, 229) 16.83%, rgb(62, 171, 179) 144.59%);
-        color: white;
-        fontsize: 15;
-      }
-      .gr-button-secondary {
-        background: rgb(228, 230, 235);
-        transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-          box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-          color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-        fontsize: 15;
-        color: black;
-      }
-      .gr-button-lg {
-        border-radius: 120px;
-      }
-      .gr-input:focus {
-        border-opacity: 0.5;
-        --tw-ring-opacity: 0;
-        --tw-border-opacity: 0.5;
-        box-shadow: none;
-        border-color: rgb(121, 46, 229);
-      }
-      .gr-input:hover {
-        border-color: rgb(121, 46, 229);
-      }
+    .gr-button-primary {
+          background: linear-gradient(
+            206.91deg, rgb(121, 46, 229)
+            16.83%, rgb(62, 171, 179) 144.59%
+          );
+          color: white;
+          fontsize: 15;
+        }
+        .gr-button-secondary {
+          background: rgb(228, 230, 235);
+          transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+            box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1)
+            0ms, border-color 250ms
+            cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+            color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+          fontsize: 15;
+          color: black;
+        }
+        .gr-button-lg {
+          border-radius: 120px;
+        }
+        .gr-input:focus {
+          border-opacity: 0.5;
+          --tw-ring-opacity: 0;
+          --tw-border-opacity: 0.5;
+          box-shadow: none;
+          border-color: rgb(121, 46, 229);
+        }
+        .gr-input:hover {
+          border-color: rgb(121, 46, 229);
+        }
     '''
 
     def __init__(self, *args, **kwargs):
@@ -84,7 +94,7 @@ class MasterpieceCreator(ServeGradio):
             clip_guidance_scale=40000,
         )
 
-        file_name = f"./img.png"
+        file_name = f'{"./img.png"}'
         result = results[0]
         result.load_uri_to_image_tensor()
         result.save_image_tensor_to_file(file_name)
@@ -101,11 +111,17 @@ class MasterpieceCreator(ServeGradio):
             self._model = self.build_model()
         fn = partial(self.predict, *args, **kwargs)
         fn.__name__ = self.predict.__name__
-        gr.Interface(fn=fn, inputs=self.inputs, outputs=self.outputs, examples=self.examples, css=self.css).launch(
-            server_name=self.host,
-            server_port=self.port,
-            enable_queue=self.enable_queue,
+        gr.Interface(
+          fn=fn, inputs=self.inputs,
+          outputs=self.outputs,
+          examples=self.examples,
+          css=self.css
+        ).launch(
+          server_name=self.host,
+          server_port=self.port,
+          enable_queue=self.enable_queue,
         )
+
 
 class RootFlow(L.LightningFlow):
     def __init__(self):
